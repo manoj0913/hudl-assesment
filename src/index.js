63a24a2998a105463fe19fee5cafa446661695be
manoj0913 <gmanoj0913@gmail.com>
2021-04-03 22:39:28 -0500

    add health check endpoint

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/avatar/:email", (req, res) => getUserImage(req, res));
+app.get("/", (req, res) => res.send("Health check request acknowledged and api is running successfull."));
 
 app.listen(PORT, () => {
 console.log(`Gravitar api is listnening on: localhost:${PORT}/`)
